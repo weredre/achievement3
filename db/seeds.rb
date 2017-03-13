a0887bd3babd4f259f0a3ca510bb5f6d1a670993
Andre Heslop <andreheslop@gmail.com>
2017-03-13 22:16:50 +0000

    4.6.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!",)
-
+Product.create(:name => "Saheeli Rai", :description => "Because infinte combos", :image_url => "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=417759&type=card", :colour => "Blue Red", :price => "10")
+Product.create(:name => "Felidar Guardian", :description => "Well you know it is a combo", :image_url => "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=423686&type=card", :colour => "White", :price => "1")
+Product.create(:name => "Goblin Guide", :description => "They are reprinting this and I'm happy", :image_url =>"http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=425921&type=card", :colour => "Red", :price => "20")
