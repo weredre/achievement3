eaf9951d985ee6190d320938d44f1905e815770a
Weredre <andreheslop@gmail.com>
2017-03-21 06:43:09 +0000

    5.3.2 Heroku email second test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
   @email = params[:email]
   @message = params[:message]
   ActionMailer::Base.mail(:from => @email,
-      :to => 'your-email@example.com',
+      :to => 'andreheslop@gmail.com',
       :subject => "A new contact form message from #{@name}",
       :body => @message).deliver_now
   end
