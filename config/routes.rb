992ff5a5e3e11e3c82bf3709a8f954a0bef5ffa5
Weredre <andreheslop@gmail.com>
2017-03-21 00:00:54 +0000

    5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
+  get 'static_pages/about' => "static_pages#about"
 
-  get 'static_pages/contact'
+  get 'static_pages/contact' => "static_pages#contact"
 
-  get 'static_pages/index'
+  get 'static_pages/index' => "static_pages#index"
   
   get "static_pages/landing_page" => "static_pages#landing_page"
   # The priority is based upon order of creation: first created -> highest priority.
