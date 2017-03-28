477c3e8f1bd1f65671cbdd2bea017a191377d25e
Weredre <andreheslop@gmail.com>
2017-03-28 00:31:23 +0000

    5.7

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ActiveRecord::Base
 def self.search(search_term)
   Product.where("name LIKE ?", "%#{search_term}%")
 end
+def highest_rating_comment
+  comments.rating_desc.first
+end
 end
