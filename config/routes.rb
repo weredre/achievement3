b76c3fa39c93816e411c5c23241cef18192a8796
Weredre <andreheslop@gmail.com>
2017-03-28 01:51:50 +0000

    5.8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  # devise_for :users
-  resources :users
+  resources :products do
+   resources :comments
+  end
   resources :products
   get 'static_pages/about' => "static_pages#about", as: 'about'
 
