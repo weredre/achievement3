d3934ae0cf133d1e6fc3bb9be4b256da6be5d5f8
Weredre <andreheslop@gmail.com>
2017-05-15 23:49:09 +0000

    5.8.3

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function(){
-   $('.rating').raty( { path: '/assets' });
-   $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+   $('.rating').raty( { path: '/assets/images' });
+   $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
  
-  $('.rated').raty({ path: '/assets',
+  $('.rated').raty({ path: '/assets/images',
      readOnly: true,
      score: function() {
      return $(this).attr('data-score');
