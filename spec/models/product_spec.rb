df7698d58e574e6317294be76c691d998c8ad6fa
Weredre <andreheslop@gmail.com>
2017-05-23 21:59:31 +0000

    6.2

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Product do
+    before do
+      @product = Product.create!(name: "race bike")
+      @user = User.create!(email: "ohno@email.com", password: "password")
+      @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+      @product.comments.create!(rating: 3, user: @user, body: "Okay bike!")
+      @product.comments.create!(rating: 5, user: @user, body: "Great bike!")
+    end
+    
+    it "returns the average rating of all comments" do
+    end
+    it"is not valid" do
+    expect(Product.new(description: "Nice bike")).not_to be_valid
+    end
+end
