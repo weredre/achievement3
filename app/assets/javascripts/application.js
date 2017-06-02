645e884f7b5c1c1764e06e0159b823edda4bb9b1
Weredre <andreheslop@gmail.com>
2017-06-02 16:36:22 +0000

    6.9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,7 @@ $(document).on('turbolinks:load', function(){
         return $(this).attr('data-score');
       }
     });
+    $('.img-zoom').elevateZoom({
+    	zoomType: "lens", lensShape: "round", lensSize: 200
+    });
 });
