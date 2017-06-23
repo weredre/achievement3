a0f5369dc0928b8d249f630a23b6571459ba13b5
Weredre <andreheslop@gmail.com>
2017-06-23 01:34:32 +0000

    byebug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,6 @@ class PaymentsController < ApplicationController
  	# POST /payments
    # POST /payments.json
  	def create
- 	  byebug
  		@product = Product.find(params[:product_id])
  		@user = current_user
  		token = params[:stripeToken]
